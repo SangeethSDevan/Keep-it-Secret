6da393f2a1120468f57233beb2f7d32e250c1fbd
Sangeeth S Devan <sangeethsdevan@gmail.com>
2024-03-03 20:41:49 +0530

    script.js update

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ function encrypt(){
     for(i=0;i<n;i++){
         let c = text[i];
         if(c===" "){
-            result=result+text[random].trim()
+            result=result+"".trim()
         }
         else{
             let code=text.charCodeAt(i);
@@ -27,7 +27,7 @@ function decrypt(){
     for(i=0;i<n;i++){
         let c= text[i];
         if(c===" "){
-            result=result+text[random]
+            result=result+"".trim()
         }
         else{
             let code = text.charCodeAt(i);
