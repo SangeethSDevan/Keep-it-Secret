31a1af21e3d91c9e54e4f737462ed5437101188e
Sangeeth S Devan <23dev500@gmail.com>
2024-09-16 21:52:05 +0530

    added backend

---
+++ public/script.js
@@ -0,0 +1,39 @@
+function encrypt(){
+    let text=document.getElementById("encrypt").value.trim()
+    document.getElementById("encrypt").value="";
+    let result=""
+    let n=text.length;
+    let random = Math.floor(Math.random()*n)
+    for(i=0;i<n;i++){
+        let c = text[i];
+        if(c===" "){
+            result=result+text[random];
+        }
+        else{
+            let code=text.charCodeAt(i);
+            c=String.fromCharCode(code+5)
+            result=result+c
+        }
+    }
+    document.getElementById("encrypted").innerHTML=result;
+}
+
+function decrypt(){
+    let text=document.getElementById("encrypt").value.trim()
+    document.getElementById("encrypt").value="";
+    let result=""
+    let n = text.length;
+    let random = Math.floor(Math.random()*n)
+    for(i=0;i<n;i++){
+        let c= text[i];
+        if(c===" "){
+            result=result+text[random];
+        }
+        else{
+            let code = text.charCodeAt(i);
+            let c=String.fromCharCode(code-5)
+            result=result+c
+        }
+    }
+    document.getElementById("encrypted").innerHTML=result;
+}
